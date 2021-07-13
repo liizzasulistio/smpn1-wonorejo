d4ee195c890daa8e39dcb271379a35d920d45ed8
vaniameith <vameitaqiyyah@gmail.com>
2021-07-13 11:56:59 +0700

    new Project

---
+++ smpn1wonorejo/app/Controllers/AchievementController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class AchievementController extends BaseController
+{
+	public function index()
+	{
+		//
+	}
+}
---
+++ smpn1wonorejo/app/Controllers/TeacherController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class TeacherController extends BaseController
+{
+	public function index()
+	{
+		//
+	}
+}

4b29b6d2ea399ca08fc467df8f2a7d28268e8814
vaniameith <vameitaqiyyah@gmail.com>
2021-08-05 22:01:34 +0700

    sidebar full bawah

--- smpn1-wonorejo/app/Views/admin/template/layout.php
+++ smpn1-wonorejo/app/Views/admin/template/layout.php
@@ -49,6 +49,9 @@
         </div>
     </div>
 
+    <script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="sidebars.js"></script>
+    
     <!-- Digunakan untuk render script yg digunakan pada form -->
     <?= $this->renderSection('script')?>
 </body>

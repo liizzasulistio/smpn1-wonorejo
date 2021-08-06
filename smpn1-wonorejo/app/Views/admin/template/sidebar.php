99bd805ae10719d5b9c39f3ad2a550b38ea73378
vaniameith <vameitaqiyyah@gmail.com>
2021-08-06 23:01:09 +0700

    - scroll sidebar

--- smpn1-wonorejo/app/Views/admin/template/sidebar.php
+++ smpn1-wonorejo/app/Views/admin/template/sidebar.php
@@ -1,28 +1,36 @@
-<!-- <div class="d-flex flex-column flex-shrink-0 p-3" style="width: 300px;  background: #084177;">
-  <a href="#" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
-      <svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
-      <img src="//icons" width="70">
-      <span class="navbar-brand flex-grow-1 text-decoration-none strong" style="color:white; text-decoration: none;">SMPN 1 WONOREJO</span>
+<main>    
+  <div class="d-flex flex-column flex-shrink-0 p-3" style="background: #084177;">
+    <a href="#" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+      <svg class="" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      <img src="<?= base_url('icons/logo.png')?>" width="70">
+      <span class="navbar-brand flex-grow-1 text-decoration-none strong" style="color:white; text-decoration: none;"><b> SMPN 1 WONOREJO</b></span>
     </a>
+
     <ul class="list-unstyled ps-0">
 
       <li class="mb-1">
         <a class="link-dark rounded" href="/dashboard">
-        <button class="btn align-items-center rounded " style="color:white; text-decoration: none;">
-          Dashboard
+        <button class="highlight btn align-items-center rounded " style="color:white; text-decoration: none;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
+            <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z"/>
+          </svg> 
+          <b> Dashboard</b>
         </button></a> 
       </li>
 
-      <li class="mb-1">
+      <!-- <li class="mb-1">
         <a class="link-dark rounded" href="/admin-profil-sekolah">
         <button class="btn align-items-center rounded " style="color:white; text-decoration: none;">
           Profil Sekolah
         </button></a> 
-      </li>
+      </li> -->
 
       <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true" style="color:white; text-decoration: none;">
-          Profil Sekolah
+        <button class="highlight btn align-items-center rounded " style="color:white; text-decoration: none;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
+            <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z"/>
+          </svg>
+          <b> Profil</b>
         </button>
 
         <div class="collapse show" id="home-collapse">
@@ -30,138 +38,84 @@
             <li><a href="/admin-sejarah" class="link-dark rounded" style="color:white; text-decoration: none;">Sejarah</a></li>
             <li><a href="/admin-visi-misi" class="link-dark rounded" style="color:white; text-decoration: none;">Visi dan Misi</a></li>
             <li><a href="/admin-fasilitas" class="link-dark rounded" style="color:white; text-decoration: none;">Fasilitas</a></li>
+            <li><a href="/admin-fasilitas" class="link-dark rounded" style="color:white; text-decoration: none;">Kepala Sekolah</a></li>
+            <li><a href="/admin-fasilitas" class="link-dark rounded" style="color:white; text-decoration: none;">Tenaga Pendidik</a></li>
+            <li><a href="/admin-fasilitas" class="link-dark rounded" style="color:white; text-decoration: none;">Tenaga Kependidikan</a></li>
             <li><a href="/admin-prestasi" class="link-dark rounded" style="color:white; text-decoration: none;">Prestasi</a></li>
           </ul>
         </div>
       </li>
 
       <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#d-collapse" aria-expanded="true" style="color:white; text-decoration: none;">
-          Pegawai
+      <button class="highlight btn align-items-center rounded " style="color:white; text-decoration: none;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
+            <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z"/>
+          </svg>
+          <b> Akademik</b>
         </button>
 
         <div class="collapse show" id="d-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="/admin-kepala-sekolah" class="link-dark rounded" style="color:white; text-decoration: none;">Kepala Sekolah</a></li>
-            <li><a href="/admin-tenaga-pendidik" class="link-dark rounded" style="color:white; text-decoration: none;">Tenaga Pendidik</a></li>
-            <li><a href="/admin-tenaga-kependidikan" class="link-dark rounded" style="color:white; text-decoration: none;">Tenaga Kependidikan</a></li>
-
+            <li><a href="/admin-kepala-sekolah" class="link-dark rounded" style="color:white; text-decoration: none;">Aturan</a></li>
+            <li><a href="/admin-tenaga-pendidik" class="link-dark rounded" style="color:white; text-decoration: none;">Daftar Siswa</a></li>
+            <li><a href="/admin-tenaga-kependidikan" class="link-dark rounded" style="color:white; text-decoration: none;">Tata Tertib</a></li>
+            <li><a href="/admin-tenaga-kependidikan" class="link-dark rounded" style="color:white; text-decoration: none;">Kalender</a></li>
           </ul>
         </div>
       </li>
 
 
       <li class="mb-1">
-  
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#c-collapse" aria-expanded="true" style="color:white; text-decoration: none;">
-         Kegiatan
+        <button class="highlight btn align-items-center rounded " style="color:white; text-decoration: none;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
+            <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z"/>
+          </svg>
+          <b> Kegiatan</b>
         </button>
 
-        <div class="collapse show" id="c-collapse">
+        <!-- <div class="collapse show" id="c-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
             <li><a href="#" class="link-dark rounded" style="color:white; text-decoration: none;">Adiwiyata</a></li>
             <li><a href="#" class="link-dark rounded" style="color:white; text-decoration: none;">Kreasi & Inovasi</a></li>
             <li><a href="#" class="link-dark rounded" style="color:white; text-decoration: none;">Ekstrakulikuler</a></li>
           </ul>
-        </div>
+        </div> -->
       </li>
-      
-      <li class="border-top my-3"></li>
 
       <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-          //disini
-        </button>
-        <div class="collapse" id="account-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="/settings" class="link-dark rounded" style="color:white; text-decoration: none;">Settings</a></li>
-            <li><a href="/logout" class="link-dark rounded" style="color:white; text-decoration: none;">Log out</a></li>
-          </ul>
-        </div>
-      </li>
-    </ul>
-</div> -->
-
-<!-- icons  -->
-<!-- disini  -->
-    
-
-<main>    
-    <div class="d-flex flex-column flex-shrink-0 p-3" style="width: 300px;  background: #084177;">
-    <a href="#" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
-      <svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
-      <img src="<?= base_url('icons/logo.png')?>" width="70">
-      <span class="navbar-brand flex-grow-1 text-decoration-none strong" style="color:white; text-decoration: none;">SMPN 1 WONOREJO</span>
-    </a>
-    <ul class="list-unstyled ps-0">
-      
-    <li class="mb-1">
-        <a class="link-dark rounded" href="/admin-profil-sekolah">
-        <button class="btn align-items-center rounded " style="color:white; text-decoration: none;">
-          Profil Sekolah
+        <a class="link-dark rounded" href="/dashboard">
+        <button class="highlight btn align-items-center rounded " style="color:white; text-decoration: none;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
+            <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z"/>
+          </svg> 
+          <b> Gallery</b>
         </button></a> 
       </li>
-
-      <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true" style="color:white; text-decoration: none;">
-          Profil Sekolah
-        </button>
-
-        <div class="collapse show" id="home-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="/admin-sejarah" class="link-dark rounded" style="color:white; text-decoration: none;">Sejarah</a></li>
-            <li><a href="/admin-visi-misi" class="link-dark rounded" style="color:white; text-decoration: none;">Visi dan Misi</a></li>
-            <li><a href="/admin-fasilitas" class="link-dark rounded" style="color:white; text-decoration: none;">Fasilitas</a></li>
-            <li><a href="/admin-prestasi" class="link-dark rounded" style="color:white; text-decoration: none;">Prestasi</a></li>
-          </ul>
-        </div>
-      </li>
-
-      <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#d-collapse" aria-expanded="true" style="color:white; text-decoration: none;">
-          Pegawai
-        </button>
-
-        <div class="collapse show" id="d-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="/admin-kepala-sekolah" class="link-dark rounded" style="color:white; text-decoration: none;">Kepala Sekolah</a></li>
-            <li><a href="/admin-tenaga-pendidik" class="link-dark rounded" style="color:white; text-decoration: none;">Tenaga Pendidik</a></li>
-            <li><a href="/admin-tenaga-kependidikan" class="link-dark rounded" style="color:white; text-decoration: none;">Tenaga Kependidikan</a></li>
-
-          </ul>
-        </div>
-      </li>
-
-
-      <li class="mb-1">
-        <!-- 1 tabel tapi kategorinya udah dibuatin -->
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#c-collapse" aria-expanded="true" style="color:white; text-decoration: none;">
-         Kegiatan
-        </button>
-
-        <div class="collapse show" id="c-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded" style="color:white; text-decoration: none;">Adiwiyata</a></li>
-            <li><a href="#" class="link-dark rounded" style="color:white; text-decoration: none;">Kreasi & Inovasi</a></li>
-            <li><a href="#" class="link-dark rounded" style="color:white; text-decoration: none;">Ekstrakulikuler</a></li>
-          </ul>
-        </div>
-      </li>
       
       <li class="border-top my-3"></li>
 
       <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-        <?= session()->get('UserUsername')?>
-        </button>
-        <div class="collapse" id="account-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="/settings" class="link-dark rounded" style="color:white; text-decoration: none;">Settings</a></li>
-            <li><a href="/logout" class="link-dark rounded" style="color:white; text-decoration: none;">Log out</a></li>
-          </ul>
-        </div>
+        <a class="link-dark rounded" href="/dashboard">
+        <button class="highlight btn align-items-center rounded " style="color:white; text-decoration: none;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+            <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+          </svg>
+          <b> Setting</b>
+        </button></a> 
+      </li>
+      
+      <li class="mb-1">
+        <a class="link-dark rounded" href="/dashboard">
+        <button class="highlight btn align-items-center rounded " style="color:white; text-decoration: none;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+            <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+          </svg>
+          <b> Log Out</b>
+        </button></a> 
       </li>
     </ul>
+
   </div>
 </main>
 
